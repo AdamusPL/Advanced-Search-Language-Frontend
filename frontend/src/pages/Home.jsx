678ce65f4244de91acf861d542cbff36639c38c3
AdamusPL <92437178+AdamusPL@users.noreply.github.com>
2025-04-25 01:39:39 +0200

    stylistic error in text...

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -20,7 +20,7 @@ export default function Home() {
         <>
             <Container id="container">
                 <h1 className='rubik-80s-fade-regular'>Hi,</h1>
-                <p className='rubik-wet-paint-regular'>Here you can search something intriuging in our database by not using regular SQL but X/Twitter manner</p>
+                <p className='rubik-wet-paint-regular'>Here you can search something intriuging in our database, not by using regular SQL, but X/Twitter manner</p>
                 <ArrowDownwardIcon id='icon'></ArrowDownwardIcon>
                 <div id='search'>
                     <SearchBar fields={fields} setFields={setFields} onSearch={handleSearchResults} />
